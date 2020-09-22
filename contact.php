5cada50ddecff85c2d9bb3250d4f8faaca284a49
zerinanan <68298360+zerinanan@users.noreply.github.com>
2020-09-23 01:59:01 +0600

    added project file

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+	session_start();
+	require_once '../config/connect.php'; 
+	if(!isset($_SESSION['email']) & empty($_SESSION['email'])){
+		header('location: login.php');
+	}
+?>
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/nav.php'; ?>
+	
+	<div class="close-btn fa fa-times"></div>
+
+	
+	<!-- SHOP CONTENT -->
+	<section id="content">
+		<div class="content-blog">
+			<div class="container">
+				<div class="row">
+					<div class="page_header text-center">
+						<h2>Shop</h2>
+						<p>Welcome to admin dashboard</p>
+					</div>
+					<div class="col-md-12">
+						
+					
+						<div class="clearfix"></div>
+						<!-- Pagination -->
+						<div class="page_nav">
+							<a href=""><i class="fa fa-angle-left"></i></a>
+							<a href="" class="active">1</a>
+							<a href="">2</a>
+							<a href="">3</a>
+							<a class="no-active">...</a>
+							<a href="">9</a>
+							<a href=""><i class="fa fa-angle-right"></i></a>
+						</div>
+						<!-- End Pagination -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<?php include 'inc/footer.php'; ?>
